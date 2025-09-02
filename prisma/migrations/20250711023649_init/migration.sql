bd9d36f7c485e93e4684ec13460d9a309e8963fd
HayatoInoue114 <k022g0087@g.neec.ac.jp>
2025-09-02 15:09:40 +0900

    Remove old migration history

--- prisma/migrations/20250711023649_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Score" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "score" INTEGER NOT NULL,
-
-    CONSTRAINT "Score_pkey" PRIMARY KEY ("id")
-);
